917bb32fa23282a74862d64e88cd9b6af6c77c4a
Jan-Paul <jpmosterdijk@gmail.com>
2019-06-12 14:23:39 +0200

    fixed merge issue

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,7 @@ let routes = require('express').Router()
 let UserController = require('../controllers/user.controller')
 
 routes.get('/users', UserController.getAllUsers)
-routes.get('/users/:id', UserController.getUserProfile)
+routes.get('/users/:id', UserController.getUser)
 routes.get('/users/:id/viewers', UserController.getViewers)
 
 routes.post('/users', UserController.createUser)
