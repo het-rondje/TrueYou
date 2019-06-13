6a933560fbcf81491bf02eb2b4be96f3a4b49366
Ricky Van den Berg <ricky.vandenberg@hotmail.nl>
2019-06-13 20:51:32 +0200

    cleanup and added routes

--- controllers/stream.controller.js
+++ controllers/stream.controller.js
@@ -1,6 +1,31 @@
 const User = require('../models/user');
 const ApiError = require('../models/ApiError');
 
+// Sends a post request to the server to make an user's stream appear online
+// Called from Android MobileClient
+function postStream(req, res, next) {
+  const { id } = req.params;
+  User.findById(id)
+    .then((user) => {
+      user.set({ online: true });
+      res.status(200).send(new ApiError('OK', 200));
+    })
+    .catch(next);
+}
+
+// Sends a post request to the server to make an user's stream appear online
+// Called from Android MobileClient
+function deleteStream(req, res, next) {
+  const { id } = req.params;
+  User.findById(id)
+    .then((user) => {
+      user.set({ online: false });
+      res.status(200).send(new ApiError('OK', 200));
+    })
+    .catch(next);
+}
+
+
 module.exports = {
 
   // Retrieves all streams currently online
@@ -13,28 +38,10 @@ module.exports = {
       .catch(next);
   },
 
-  // Sends a post request to the server to make an user's stream appear online
-  // Called from Android MobileClient
-  postStream(req, res, next) {
-    const { id } = req.params;
-    const streamKey = req.body;
-    User.findById(id)
-      .then((user) => {
-        user.set({ online: true, streamKey });
-        res.status(200).send(new ApiError('OK', 200));
-      })
-      .catch(next);
-  },
-
-  // Sends a post request to the server to make an user's stream appear online
-  // Called from Android MobileClient
-  deleteStream(req, res, next) {
-    const { id } = req.params;
-    User.findById(id)
-      .then((user) => {
-        user.set({ online: false, streamKey: '' });
-        res.status(200).send(new ApiError('OK', 200));
-      })
-      .catch(next);
+  controlStream(req, res, next) {
+    if (req.body.online) {
+      return deleteStream(req, res, next);
+    }
+    return postStream(req, res, next);
   },
 };
--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,15 @@
 // eslint-disable-next-line new-cap
 const routes = require('express').Router();
 const UserController = require('../controllers/user.controller');
+const StreamController = require('../controllers/stream.controller');
 
 routes.get('/users', UserController.getAllUsers);
 routes.get('/users/:id', UserController.getUser);
 routes.get('/users/:id/viewers', UserController.getViewers);
 
+routes.post('/users/:id', UserController.postLoginUser);
 routes.post('/users', UserController.createUser);
+
+routes.post('/streams/:id/', StreamController.postStream);
 
 module.exports = routes;
