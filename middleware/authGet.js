8823c3f7f03a7765cd5065f8247763237aa64f4c
Jan Belterman <jjabelte@avans.nl>
2019-06-14 13:52:54 +0200

    Added various middleware to check authentication and integrity

---
+++ middleware/authGet.js
@@ -0,0 +1,22 @@
+/*
+    This module validates an incomming get requests
+    It needs:
+    - Headers
+        - userId
+        - signature
+        - checkData
+*/
+
+const requestCheck = require('../auth/requestCheck')
+
+module.exports = async (req, res, next) => {
+    // Get data
+    const userId = req.headers.userId
+    const signature = req.headers.signature
+    const checkData = req.headers.checkData
+    if (!userId || !signature || !checkData) return res.status(400).send('Provide userId, signature & checkData')
+    // Verify
+    const result = requestCheck(userId, signature, checkData)
+    if (!result) return res.status(401).send('Unauthorized')
+    next()
+}
