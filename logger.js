dd4ec2b7a5bdbda398b24187e4b4a1fe18a88491
Floris Botermans <fboterma@avans.nl>
2019-06-14 12:59:34 +0200

    Added db

--- logger.js
+++ logger.js
@@ -1,20 +1,21 @@
 const winston = require('winston');
+require('winston-mongodb');
 
 const logFormat = winston.format.combine(
-    winston.format.colorize(),
     winston.format.timestamp(),
-    winston.format.align(),
     winston.format.printf(
         info => `${info.timestamp} ${info.message}`,
     )
 );
 
+const dbOptions = {
+    db: 'mongodb://dbadmin:U1dI3QD34qRMXs@ds237337.mlab.com:37337/rondje',
+};
+
 winston.loggers.add('customLogger', {
     format: logFormat,
     transports: [
-        new winston.transports.File({
-            filename: 'test.log'
-        }),
+        new winston.transports.MongoDB(dbOptions),
         new winston.transports.Console({
             level: 'info',
         })
@@ -22,7 +23,5 @@ winston.loggers.add('customLogger', {
 });
 
 const logger = winston.loggers.get('customLogger');
-
-logger.info('Testing');
 
 module.exports = logger;
